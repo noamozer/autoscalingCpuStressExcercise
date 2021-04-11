8d609a0d55db7951a15d1621aa2b9ec506f13b96
noamozer <50733233+noamozer@users.noreply.github.com>
2021-04-11 14:47:26 +0300

    Update consts.py

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 from helpers import readFile, base64EncodeScript
 
-# define env vars - public Linux us-west-2 AMI, ELB Name
+# define public Linux us-west-2 AMI, ELB Name
 US_WEST_2_LINUX_AMI = "ami-0518bb0e75d3619ca"
 ELB_NAME = "GuestTestELB"
 
